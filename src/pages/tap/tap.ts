dada2bad39f16caa5e44307e90f02d08dfd06a4c
sahzinsocorp <sah.insocorp@gmail.com>
2018-10-23 16:33:45 -0500

    UpLoad-17-SAH
    Se creo la pagina tap

--- src/pages/tap/tap.ts
+++ src/pages/tap/tap.ts
@@ -15,10 +15,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TapPage {
 
-  
+  Minute:number;
+  Seconds:number;
+  Paused: boolean;
+  Click: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) 
+  {
+    this.Click=0;
+    this.resetTimer();
+    setInterval(() => this.tick(), 1000);
   }
+
+  resetTimer(): void {
+    this.Minute = 0;
+    this.Seconds=59;
+  }
+
+  private tick(): void{
+    if(--this.Seconds<0){
+        this.Seconds=59;
+      }
+    }
+
+    togglePause(): void{
+
+    }
+
+    Clicks(){
+      this.Click=this.Click + 1;
+    }
+  
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TapPage');
