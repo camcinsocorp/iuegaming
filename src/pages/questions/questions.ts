a99a3136e085480393f2615bad4e8fcc555600f6
dacjinsocorp <dacj.insocorp@gmail.com>
2018-10-25 12:06:23 -0500

    DACJ - Update

--- src/pages/questions/questions.ts
+++ src/pages/questions/questions.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { GameServicesProvider } from '../../providers/services/game-services.ts/game-services';
+import { GlobalProvider } from '../../providers/global/global';
 
 /**
  * Generated class for the QuestionsPage page.
@@ -15,11 +17,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class QuestionsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  Question: any;
+  Answer1: any;
+  Answer2: any;
+  Answer3: any;
+  Answer4: any;
+  CorrectAnswer: any;
+  ScoreRewarded: any
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private gameServicesProvider: GameServicesProvider, public global: GlobalProvider) {
+      this.getQuestion();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad QuestionsPage');
+  }
+
+  getQuestion() {
+    this.gameServicesProvider.getLevels({ levelNumber: this.global.ToLevel})
+          .then(data => {
+            this.Question = data.question;
+            this.Answer1 = data.answer1;
+            this.Answer2 = data.answer2;
+            this.Answer3 = data.answer3;
+            this.Answer4 = data.answer4;
+            this.CorrectAnswer = data.correctAnswer;
+            this.ScoreRewarded = data.scoreRewarded;
+          })
   }
 
 }
